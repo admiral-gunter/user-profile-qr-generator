069c530649bbf7c537955068b67bff534cdb78f1
Rafli <muhammad.rafli241103@gmail.com>
2023-05-14 15:36:02 +0700

    new ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,69 +1,80 @@
 
 @include('template.base')
 
-<style>
-/* .color-change {
-animation-name: color-change;
-animation-duration: 2s;
-animation-iteration-count: infinite;
-}
-
-@keyframes color-change {
-  0% {
-    background-color: rgb(252, 252, 158);
-  }
-  50% {
-    background-color: rgb(207, 207, 107);
-  }
-  100% {
-    background-color: rgb(225, 233, 122);
-  }
-} */
-
-</style>
-
 <div>
-    <div style="display: flex; justify-content:center">
-    
-        <div style="background:rgb(229, 229, 97);width: 50%; height:100vh; padding:50px">
-            <div>
+    <div style="">
+        <div style="background:rgb(243, 243, 133);width: 100%; height: 100vh; padding:50px; display:flex; justify-content:space-between;">
+            <div style="width:40%">
                 <div style="display:flex; justify-content:center">
-                    <img src="https://picsum.photos/200" style="width: 25%; height:25%; object-fit:contain; border-radius:100%"/>
+                    <img src="https://picsum.photos/200" style="width: 150px; height: 150px; object-fit:contain; border-radius:100%"/>
                 </div>
                 <div style="text-align: center">
                     <h4>{{$user_data->name}}</h4>
                     <h5>{{$user_data->email}}</h5>
                 </div>
             </div>
-        </div>
-        <div style="width:50%; padding:50px">
-            <div id="listSocial">
-                @foreach ($data as $key=> $item)
-                    <div class="form-group mb-3" style="display: flex; justify-content:between" id="item-social-{{$key+1}}">
-                        <div  style="width:75%">
-                            <input type="text" placeholder="Enter Type.." value="{{$item->type}}" style="outline: none; border:none" id="input-type-{{$key+1}}"/> 
-                            <input  class="form-control" placeholder="Enter Link.." value="{{$item->link}}" id="input-link-{{$key+1}}"/>
-                        </div>
-                        @if ($whom_id == $user_id)
-                        <div class="mt-4" style="margin: auto">
-                            <p style="font-weight: bold; color:white; padding:10px; background:rgb(255, 108, 108); border-radius:5px" onclick="removeThis({{$key+1}})">X</p>
-                        </div>
-                        @endif
-                    </div>
-                @endforeach
-            </div>
-    
+            <div style="width:60%">
+                <ul style="list-style:none">
+                    <li>
+                        <p>She/Her</p>
+                    </li>
+                    <li>
+                        <p style="font-weight: bold; margin:0">Nationality</p>
+                        <p>
+                            <span class="fi fi-jp"></span>
+                        </p>
+                    </li>
+                    <li>
+                        <p style="font-weight: bold; margin:0">Bio</p>
+                        <p style="width: 50%">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, suscipit, atque molestias inventore beatae sunt provident at corrupti nesciunt vero amet sequi perferendis dolor placeat voluptatem consequuntur hic molestiae illo!
+                        </p>
+                    </li>
+                </ul>
 
-            @if ($whom_id == $user_id)
-                
-            <button  class="btn btn-success" id="socialsAdd">Add</button>
-            <button  class="btn btn-primary" onclick="submit()">Submit</button>
-            @endif
-            <button  class="btn btn-secondary" onclick="generateQR()">generate QR</button>
-            
-            <div id="qrcode"></div>
-    
+                <div>
+                    <ul style="list-style:none">
+                        @foreach ($data as $key=> $item)
+                            <li>
+                                <a href="{{$item->link}}" target="_blank">{{$item->type}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+          
         </div>
+        @if ($whom_id == $user_id)
+            <div style="padding:50px">
+                <div id="listSocial">
+                    @foreach ($data as $key=> $item)
+                        <div class="form-group mb-3" style="display: flex; justify-content:between" id="item-social-{{$key+1}}">
+                            <div  style="width:75%">
+                                <input type="text" placeholder="Enter Type.." value="{{$item->type}}" style="outline: none; border:none" id="input-type-{{$key+1}}"/> 
+                                <input  class="form-control" placeholder="Enter Link.." value="{{$item->link}}" id="input-link-{{$key+1}}"/>
+                            </div>
+                            @if ($whom_id == $user_id)
+                            <div class="mt-4" style="margin: auto">
+                                <p style="font-weight: bold; color:white; padding:10px; background:rgb(255, 108, 108); border-radius:5px" onclick="removeThis({{$key+1}})">X</p>
+                            </div>
+                            @endif
+                        </div>
+                    @endforeach
+                </div>
+        
+
+                @if ($whom_id == $user_id)
+                    
+                <button  class="btn btn-success" id="socialsAdd">Add</button>
+                <button  class="btn btn-primary" onclick="submit()">Submit</button>
+                @endif
+                <button  class="btn btn-secondary" onclick="generateQR()">generate QR</button>
+                
+                <div id="qrcode"></div>
+        
+            </div>
+        @endif
+
     </div>
 </div>
 
@@ -131,12 +142,9 @@ function submit() {
                 }
             )
         }
-
-        
     }
 
     console.log(itemsValue)
-
 
     var request = new Request('http://localhost:8000/api/v1/user-socials/add', {
         method: 'POST',
